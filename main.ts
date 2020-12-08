88646df9664db88474baa10bcec932e8f45b9180
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-08 09:35:39 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.B, function () {
     sol = pins.analogReadPin(AnalogPin.P3)
     basic.pause(100)
     pins.digitalWritePin(DigitalPin.P7, 0)
+    // Ajuster cette valeur dans tous ce calcul avec la valeur de la variable « sol » lorsque la sonde n'est pas dans le sol.
     pourcentage = Math.round((sol - 600) * 100 / (1023 - 600))
     basic.showString("T=" + Température + "C")
     basic.pause(1000)
@@ -28,6 +29,7 @@ basic.forever(function () {
     sol = pins.analogReadPin(AnalogPin.P3)
     basic.pause(100)
     pins.digitalWritePin(DigitalPin.P7, 0)
+    // Ajuster cette valeur dans tous ce calcul avec la valeur de la variable « sol » lorsque la sonde n'est pas dans le sol.
     pourcentage = Math.round((sol - 600) * 100 / (1023 - 600))
     // Si l'humidité du sol est inférieure à 95 % arroser jusqu'à ce que l'humidité soit supérieure à 98%.
     // Lorsque l'humidité du sol est supérieure à 98% arrêter d'arroser jusqu'à ce que l'humidité soit inférieure à 95 %.
@@ -46,9 +48,11 @@ basic.forever(function () {
 })
 basic.forever(function () {
     Température = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P0)
-    basic.pause(1000)
+    basic.pause(100)
     radio.sendValue("T ", Température)
+    // Ajuster cette valeur en fonction de vos paramètres expérimentaux
     if (Température >= 24) {
+        // Auster cette valeur de l'angle d'ouverture pour que la fenêtre reste ouverte sans courant.
         while (Angle_fenetre > 35) {
             Angle_fenetre += -1
             servos.P1.setAngle(Angle_fenetre)
@@ -56,6 +60,7 @@ basic.forever(function () {
         }
         servos.P1.stop()
     } else {
+        // Auster cette valeur de l'angle de fermeture pour que la fenêtre ferme correctement.
         while (Angle_fenetre < 160) {
             Angle_fenetre += 1
             servos.P1.setAngle(Angle_fenetre)
